b5b17356e3e6d0fe1456a5eadd94b47334dd8754
Abhishek Sharma <abhisharma4950@gmail.com>
2024-10-02 08:45:29 -0400

    vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,12 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Alias for src directory
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: 'dist', // Output directory for build
-  },
-});
+})
