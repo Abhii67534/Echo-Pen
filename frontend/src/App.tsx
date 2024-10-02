72b90daf01efdb5059f2d707807d4cd7ce7d943b
Abhishek Sharma <abhisharma4950@gmail.com>
2024-10-02 09:39:03 -0400

    used createBrowserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,36 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Signin } from './pages/Signin'
-import { Blog } from './pages/Blog'
-import { Signup } from './pages/Signup'
-import { Home } from './pages/Home'
-import { RecoilRoot } from 'recoil'
-import { BlogPost } from './pages/BlogPost'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Blog } from "./pages/Blog";
+import { Signup } from "./pages/Signup";
+import { Home } from "./pages/Home";
+import { BlogPost } from "./pages/BlogPost";
+import "./index.css";
 
-function App() {
+// Define the router using createBrowserRouter
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
+  },
+  {
+    path: "/blog-post",
+    element: <BlogPost />,
+  },
+]);
 
-  return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog-post" element={<BlogPost />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
-  )
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App
