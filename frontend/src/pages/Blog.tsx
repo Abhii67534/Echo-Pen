8cbcf4dd00890e5e02f9a2f9c9a2c06c08f61155
Abhishek Sharma <abhisharma4950@gmail.com>
2024-10-12 09:41:36 -0400

    Added jimmy link

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -100,117 +100,129 @@ export const Blog = () => {
 
   return (
     <div className="bg-rose-50 min-h-screen w-screen overflow-x-hidden">
-    <div className="tab:pl-10 xs:pt-2 xs:pr-4 tab:pt-4 xs:pl-5 border-b-2 border-gray-500 pb-5 ">
-      <nav className="flex justify-center">
-        <div className="container flex justify-between items-center">
-          <div className="flex flex-row">
-            <div className="xs:text-lg tab:text-2xl font-bold font-im-fell-english">
-              <Link to="/">Echo-Pen</Link>
+      <div className="tab:pl-10 xs:pt-2 xs:pr-4 tab:pt-4 xs:pl-5 border-b-2 border-gray-500 pb-5 ">
+        <nav className="flex justify-center">
+          <div className="container flex justify-between items-center">
+            <div className="flex flex-row">
+              <div className="xs:text-lg tab:text-2xl font-bold font-im-fell-english">
+                <Link to="/">Echo-Pen</Link>
+              </div>
+
+              <div className="ml-10 hidden sm:block">
+                <Input
+                  className="rounded-full"
+                  placeholder="Search"
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+              </div>
             </div>
-
-            <div className="ml-10 hidden sm:block">
-              <Input
-                className="rounded-full"
-                placeholder="Search"
-                onChange={(e) => setSearch(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="flex items-center">
-            <Button
-              className="rounded-full ml-10 xs:w-[70px] xs:h-[30px] tab:w-[90px] tab:h-[35px]"
-              onClick={handleWrite}
-            >
-              <img
-                className="tab:w-[15px] tab:h-[15px] hidden tab:block mr-2"
-                src="/images/pen.png"
-                alt="Write Icon"
-              />
-              Write
-            </Button>
-            <Button
-              className="rounded-full tab:ml-10 xs:ml-4 xs:w-[70px] xs:h-[30px] tab:w-[80px] tab:h-[38px]"
-              onClick={handleLogout}
-            >
-              Logout
-            </Button>
-          </div>
-        </div>
-      </nav>
-    </div>
-
-    <div className="flex flex-col md:flex-row min-h-screen">
-      {/* LEFT SIDE */}
-      <div className="flex flex-col md:w-3/4 md:pt-5 items-center">
-        {loading ? (
-          <>
-            <Skeleton className="h-24 w-3/4 mb-4" />
-            <Skeleton className="h-24 w-3/4 mb-4" />
-            <Skeleton className="h-24 w-3/4 mb-4" />
-            <Skeleton className="h-24 w-3/4 mb-4" />
-            <Skeleton className="h-24 w-3/4 mb-4" />
-            <Skeleton className="h-24 w-3/4 mb-4" />
-            <Skeleton className="h-24 w-3/4 mb-4" />
-          </>
-        ) : filteredBlogs.length > 0 ? (
-          filteredBlogs.map((blogPost) => (
-            <BlogCard
-              key={blogPost.id}
-              id={blogPost.id}
-              title={blogPost.title}
-              content={blogPost.content}
-              avatar={blogPost.avatar}
-              author={blogPost.author}
-              likes={blogPost.likes}
-              date={blogPost.date}
-              authorId={blogPost.authorId}
-            />
-          ))
-        ) : (
-          <div className="text-4xl text-red-600 font-im-fell-english mt-20">
-            No blogs found.
-          </div>
-        )}
-      </div>
-
-      {/* Vertical Line Divider */}
-      <div className="w-[2px] bg-gray-500"></div>  {/* This is the new vertical line */}
-
-      {/* RIGHT SIDE */}
-      <div className="w-full md:w-1/4 min-h-screen pl-10 pt-10 hidden lg:flex flex-col">
-        <div className="w-[300px] h-[300px] rounded-lg bg-blue-200">
-          <div className="font-bold flex justify-center mb-10 pt-5">
-            Writing on Echo-Pen
-          </div>
-          <div className="flex justify-center mb-2 font-medium">
-            Expert writing advice
-          </div>
-          <div className="flex justify-center mb-3 font-medium">
-            Grow your readership
-          </div>
-          <div className="flex justify-center font-medium">
-            <Button className="rounded-full" onClick={handleWrite}>
-              Start Writing
-            </Button>
-          </div>
-        </div>
-
-        <div className="mt-10 ml-2">
-          <div className="font-bold mb-5">Liked By Others</div>
-          <div className="flex flex-col">
-            {randomAuthors.map((author) => (
+            <div className="flex items-center">
               <Button
-                className="w-[150px] mb-4"
-                key={author}
-                onClick={() => displayRandom(author)}
+                className="rounded-full ml-10 xs:w-[70px] xs:h-[30px] tab:w-[90px] tab:h-[35px]"
+                onClick={handleWrite}
               >
-                {author}
+                <img
+                  className="tab:w-[15px] tab:h-[15px] hidden tab:block mr-2"
+                  src="/images/pen.png"
+                  alt="Write Icon"
+                />
+                Write
               </Button>
-            ))}
+              <Button
+                className="rounded-full tab:ml-10 xs:ml-4 xs:w-[70px] xs:h-[30px] tab:w-[80px] tab:h-[38px]"
+                onClick={handleLogout}
+              >
+                Logout
+              </Button>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <div className="flex flex-col md:flex-row min-h-screen">
+        {/* LEFT SIDE */}
+        <div className="flex flex-col md:w-3/4 md:pt-5 items-center">
+          {loading ? (
+            <>
+              <Skeleton className="h-24 w-3/4 mb-4" />
+              <Skeleton className="h-24 w-3/4 mb-4" />
+              <Skeleton className="h-24 w-3/4 mb-4" />
+              <Skeleton className="h-24 w-3/4 mb-4" />
+              <Skeleton className="h-24 w-3/4 mb-4" />
+              <Skeleton className="h-24 w-3/4 mb-4" />
+              <Skeleton className="h-24 w-3/4 mb-4" />
+            </>
+          ) : filteredBlogs.length > 0 ? (
+            filteredBlogs.map((blogPost) => (
+              <BlogCard
+                key={blogPost.id}
+                id={blogPost.id}
+                title={blogPost.title}
+                content={blogPost.content}
+                avatar={blogPost.avatar}
+                author={blogPost.author}
+                likes={blogPost.likes}
+                date={blogPost.date}
+                authorId={blogPost.authorId}
+              />
+            ))
+          ) : (
+            <div className="text-4xl text-red-600 font-im-fell-english mt-20">
+              No blogs found.
+            </div>
+          )}
+        </div>
+        {/* Vertical Line Divider */}
+        <div className="w-[2px] bg-gray-500"></div>{" "}
+        {/* This is the new vertical line */}
+        {/* RIGHT SIDE */}
+        <div className="w-full md:w-1/4 min-h-screen pl-10 pt-10 hidden lg:flex flex-col items-center mr-10">
+          <div className="w-[300px] h-[300px] rounded-lg bg-blue-200">
+            <div className="font-bold flex justify-center mb-10 pt-5">
+              Writing on Echo-Pen
+            </div>
+            <div className="flex justify-center mb-2 font-medium">
+              Expert writing advice
+            </div>
+            <div className="flex justify-center mb-3 font-medium">
+              Grow your readership
+            </div>
+            <div className="flex justify-center font-medium">
+              <Button className="rounded-full" onClick={handleWrite}>
+                Start Writing
+              </Button>
+            </div>
+          </div>
+
+          <div className="mt-10 ml-2">
+            <div className="font-bold mb-5">Random Picks</div>
+            <div className="flex flex-col">
+              {randomAuthors.map((author) => (
+                <Button
+                  className="w-[150px] mb-4"
+                  key={author}
+                  onClick={() => displayRandom(author)}
+                >
+                  {author}
+                </Button>
+              ))}
+            </div>
+          </div>
+          <div className="w-[300px] h-[200px] rounded-lg bg-red-200 mt-10">
+            <div className="font-bold flex justify-center mb-10 pt-5">
+              Not sure what to write?
+            </div>
+            <div className="flex justify-center mb-3 font-medium">
+              <p>Ask&nbsp; </p> <p className="italic">Jimmy</p>
+            </div>
+            <div className="flex justify-center font-medium">
+              <Button className="rounded-full" onClick={handleWrite}>
+                Go To Jimmy
+              </Button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
